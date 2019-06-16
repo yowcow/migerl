4986d88e7e6ecc0ded034f360faf0ebb83ae5089
yowcow <yowcow@cpan.org>
2019-06-16 20:37:43 +0900

    fix

--- src/migerl_db.erl
+++ src/migerl_db.erl
@@ -4,7 +4,11 @@
     start/1,
     stop/1,
     query/3,
-    tx_queries/2
+    queries/2,
+    tx_queries/2,
+    is_applied/2,
+    apply_query/2,
+    unapply_query/2
 ]).
 
 start(Config) ->
@@ -22,23 +26,51 @@ stop({mysql, Pid}) ->
 
 query({mysql, Pid}, Query, Args) ->
     Ret = mysql:query(Pid, Query, Args),
-    case Ret of
+    Result = case Ret of
         ok         -> ok;
         {ok, _}    -> Ret;
         {ok, _, _} -> Ret;
         Err ->
             error(io_lib:format("mysql query failed: ~p", [Err]))
-    end.
+    end,
+    Result.
 
-tx_queries({mysql, Pid} = Conn, Queries) ->
-    try mysql:transaction(Pid, fun() -> queries(Conn, Queries) end) of
-        {atomic, Result} ->
-            Result
-    catch
-        error:{implicit_commit, _} -> ok
-    end.
-
-queries(_, []) -> done;
+queries(_, []) -> ok;
 queries(Conn, [{Query, Args} | Rem]) ->
     query(Conn, Query, Args),
     queries(Conn, Rem).
+
+tx_queries({mysql, Pid} = Conn, Queries) ->
+    try mysql:transaction(Pid, fun() -> queries(Conn, Queries) end) of
+        {atomic, Result} -> Result
+    catch
+        error:Err ->
+            throw(Err)
+    end.
+
+is_applied(Conn, File) ->
+    {ok, _, [[Count]]} = query(
+        Conn,
+        "SELECT count(*) FROM migrations "
+        "WHERE id = ? AND applied_at IS NOT NULL",
+        [File]
+    ),
+    case Count of
+        0 -> false;
+        _ -> true
+    end.
+
+apply_query({mysql, _}, File) ->
+    {
+        "INSERT INTO migrations "
+        "SET id = ?, applied_at = NOW() "
+        "ON DUPLICATE KEY UPDATE applied_at = NOW() ",
+        [File]
+    }.
+
+unapply_query(_, File) ->
+    {
+        "DELETE FROM migrations "
+        "WHERE id = ?",
+        [File]
+    }.
--- test/migerl_db_tests.erl
+++ test/migerl_db_tests.erl
@@ -10,42 +10,78 @@ start_stop_test() ->
 setup() ->
     Config = migerl_config:load("default", "test/test.config"),
     Conn = migerl_db:start(Config),
+    ok = migerl_db:query(Conn, "DROP TABLE IF EXISTS migrations", []),
     ok = migerl_db:query(Conn, "DROP TABLE IF EXISTS example_table", []),
+    ok = migerl_init:dispatch(Conn, [{dir, "/tmp/migerl-hoge"}]),
     Conn.
 
 cleanup(Conn) ->
     migerl_db:stop(Conn).
 
-tx_query_test_() ->
+notx_tx_queries_test_() ->
     {setup, fun setup/0, fun cleanup/1, fun(Conn) ->
         [
-            fun() ->
-                Query = "CREATE TABLE example_table ( "
-                        "id int(11) unsigned not null, "
-                        "name varchar(255) not null, "
-                        "PRIMARY KEY(id) "
-                        ") Engine=InnoDB default charset=utf8;",
-                ok = migerl_db:tx_queries(Conn, [{Query, []}])
-            end,
-            fun() ->
-                Queries = [
-                    {"INSERT INTO example_table (id, name) VALUES (1, 'hoge');", []},
-                    {"INSERT INTO example_table (id, name) VALUES (1, 'fuga');", []}
-                ],
-                ok = try migerl_db:tx_queries(Conn, Queries) of
-                    should_fail -> should_fail
-                catch
-                    error:_ -> ok
+            {
+                "create table without transaction",
+                fun() ->
+                    Query = "CREATE TABLE example_table ( "
+                            "id int(11) unsigned not null, "
+                            "name varchar(255) not null, "
+                            "PRIMARY KEY(id) "
+                            ") Engine=InnoDB default charset=utf8;",
+                    ok = migerl_db:queries(Conn, [{Query, []}])
                 end
+            },
+            {
+                "insert with dupe primary key in transaction",
+                fun() ->
+                    Queries = [
+                        {"INSERT INTO example_table (id, name) VALUES (1, 'hoge');", []},
+                        {"INSERT INTO example_table (id, name) VALUES (1, 'fuga');", []}
+                    ],
+                    ok = try migerl_db:tx_queries(Conn, Queries) of
+                        should_fail -> should_fail
+                    catch
+                        error:_ ->
+                            ok
+                    end
+                end
+            },
+            {
+                "insert succeeds in transaction",
+                fun() ->
+                    Queries = [
+                        {"INSERT INTO example_table (id, name) VALUES (1, 'hoge');", []},
+                        {"INSERT INTO example_table (id, name) VALUES (2, 'fuga');", []}
+                    ],
+                    ok = migerl_db:tx_queries(Conn, Queries),
+                    {ok, _, Result} = migerl_db:query(Conn, "SELECT * FROM example_table", []),
+                    ?assertEqual([[1, <<"hoge">>], [2, <<"fuga">>]], Result)
+                end
+            }
+        ]
+    end}.
+
+apply_unapply_test_() ->
+    {setup, fun setup/0, fun cleanup/1, fun(Conn) ->
+        File = "hoge_test",
+        [
+            fun() ->
+                ?assertEqual(false, migerl_db:is_applied(Conn, File))
             end,
             fun() ->
-                Queries = [
-                    {"INSERT INTO example_table (id, name) VALUES (1, 'hoge');", []},
-                    {"INSERT INTO example_table (id, name) VALUES (2, 'fuga');", []}
-                ],
-                done = migerl_db:tx_queries(Conn, Queries),
-                {ok, _, Result} = migerl_db:query(Conn, "SELECT * FROM example_table", []),
-                ?assertEqual([[1, <<"hoge">>], [2, <<"fuga">>]], Result)
+                {Query, Args} = migerl_db:apply_query(Conn, File),
+                ok = migerl_db:query(Conn, Query, Args)
+            end,
+            fun() ->
+                ?assertEqual(true, migerl_db:is_applied(Conn, File))
+            end,
+            fun() ->
+                {Query, Args} = migerl_db:unapply_query(Conn, File),
+                ok = migerl_db:query(Conn, Query, Args)
+            end,
+            fun() ->
+                ?assertEqual(false, migerl_db:is_applied(Conn, File))
             end
         ]
     end}.
