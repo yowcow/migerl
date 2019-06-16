d6f57fe67c4a2730620b2dd44e44cc6a553e5470
yowcow <yowcow@cpan.org>
2019-06-16 20:58:32 +0900

    fix

--- test/files/file3.sql
+++ test/files/file3.sql
@@ -8,7 +8,7 @@ INSERT INTO member (id, name) VALUES
 INSERT INTO member_password (member_id, password_hash, password_salt) VALUES
 (1, "cd303f921f510ff6b5daa1d6f0747e2c7be978a6", "fugafuga"),
 (2, "295191167f8092e7f7221d2e0416d326ab9131a6", "foofoo"),
-(2, "80cca7e0d00a8e700b383a449aba59c1f4b401b1", "barbar");
+(3, "80cca7e0d00a8e700b383a449aba59c1f4b401b1", "barbar");
 
 -- +migrate Down
 
