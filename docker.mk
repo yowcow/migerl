b888f2992e33f0515a2ad24495c99fe845f6071e
yowcow <yowcow@cpan.org>
2020-03-14 19:29:55 +0900

    postgres default password

--- docker.mk
+++ docker.mk
@@ -1,6 +1,8 @@
 MYSQL := mysql:5.7
 POSTGRES := postgres:11
 
+PASSWORD ?= "Passw0rd"
+
 all: pull
 
 pull:
@@ -28,7 +30,7 @@ postgres.cid:
 	docker run --rm -d \
 		--cidfile $@ \
 		--name migerl-postgres \
-		-e POSTGRES_PASSWORD="" \
+		-e POSTGRES_PASSWORD=$(PASSWORD) \
 		-p 5432:5432 \
 		$(POSTGRES)
 
@@ -39,4 +41,25 @@ stop-%:
 	[ -f $*.cid ] && docker stop $$(cat $*.cid) || true
 	rm -f $*.cid
 
-.PHONY: all pull pull-* start stop stop-*
+create-db: create-mysql create-postgres
+
+create-mysql:
+	RET=""; TRIES=30; \
+	until [ "$$RET" != "" ] || [ $$TRIES -eq 0 ]; do \
+		sleep 1; \
+		RET=$$(echo "select 'ok'" | mysql -h127.0.0.1 -uroot); \
+		TRIES=$$(($$TRIES - 1)); \
+	done || true;
+	echo "create database if not exists migerl_test default charset=utf8;" \
+		| mysql -h127.0.0.1 -uroot
+
+create-postgres:
+	RET=""; TRIES=30; \
+	until [ "$$RET" != "" ] || [ $$TRIES -eq 0 ]; do \
+		sleep 1; \
+		RET=$$(PGPASSWORD=$(PASSWORD) psql -c "select 1;" -U postgres -h 127.0.0.1 -p 5432); \
+		TRIES=$$(($$TRIES - 1)); \
+	done || true;
+	PGPASSWORD=$(PASSWORD) psql -c "create database migerl_test;" -U postgres -h 127.0.0.1 -p 5432
+
+.PHONY: all pull pull-* start stop stop-* create-db create-mysql create-postgres
